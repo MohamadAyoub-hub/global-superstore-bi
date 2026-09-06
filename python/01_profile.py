5038d5e7de9e8a025ce7ca994d320bcfb9588c42
Mohammad <mhmd4work.1@gmail.com>
2026-09-06 18:56:53 +0300

    Adding profile function

--- python/01_profile.py
+++ python/01_profile.py
@@ -4,19 +4,17 @@ df["Clean_order_date"]=pd.to_datetime(df["Clean_order_date"], format="mixed")
 df["Clean_Ship_Date"]=pd.to_datetime(df["Clean_Ship_Date"], format="mixed")
 
 print(df.head())
-print(df.info())
-print(df.shape)
-print(df.columns)
-print(df.isna().sum())
-print(df["customer_name"].nunique())
-print(df["product_id"].nunique())
-print(df["category"].nunique())
-print(df["sub_category"].nunique())
-print(df["region"].nunique())
-print(df["market"].nunique())
-print(df["ship_mode"].nunique())
-print(df["order_id"].nunique())
-print(len(df))
+def profile_dataframe(df):
+    profile= pd.DataFrame({
+        "dtypes": df.dtypes,
+        "missing_values": df.isna().sum(),
+        "missing_percent":df.isna().mean()*100,
+        "unique":df.nunique(),
+        "duplicated":df.duplicated().sum()
+    })
+    return profile
+profile=profile_dataframe(df)
+print(profile)
 df.groupby("order_id").size().sort_values(ascending=False).head()
 print(df)
 print(df["Clean_order_date"].min())
@@ -24,4 +22,3 @@ print(df["Clean_order_date"].max())
 print(df["Clean_Ship_Date"].min())
 print(df["Clean_Ship_Date"].max())
 print(df[df["profit"]<0])
-print(df[df["customer_name"].isna()])
